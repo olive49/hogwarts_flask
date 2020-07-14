7c18fafaf48b419c1d3bc8c72030b6efae829c81
Becca <60694049+olive49@users.noreply.github.com>
2020-07-14 15:08:37 +0300

    String

--- Human.py
+++ Human.py
@@ -3,7 +3,7 @@ class Human:
         self._first_name = first_name
         self._last_name = last_name
         self._email = email
-        self.__password = password
+        self._password = password
 
 
 class Student(Human):
@@ -15,6 +15,13 @@ class Student(Human):
         self.last_update = last_update
         self.existing_magic_skills = existing_magic_skills
         self.desired_magic_skills = desired_magic_skills
+
+    def __str__(self):
+        return '"Student ID": {},\n"First Name": {},\n"Last Name": {},\n"Email": {}\n"Password": {}' \
+               '\n"Creation Time": {}, \n"Last Update": {}, \n"Existing Magic Skills": {}, ' \
+               '\n"Desired Magic Skills": {} '.format(self._id, self._first_name, self._last_name, self._email,
+                                                      self._password, self.creation_time, self.last_update,
+                                                      self.existing_magic_skills, self.desired_magic_skills)
 
 
 class Admin(Human):
