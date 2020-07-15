e0ec9ca5e1fc3d3761108d92d0ceedb3f5f503f2
Becca <60694049+olive49@users.noreply.github.com>
2020-07-15 09:32:08 +0300

    Persist Users

--- DataLayer.py
+++ DataLayer.py
@@ -1,3 +1,5 @@
+import os
+import pathlib
 from Human import Student
 import json
 
@@ -33,10 +35,30 @@ class DataLayer:
         if student_email not in self._students_dict.keys():
             raise ValueError("Student email does not exist")
 
-        student = self._students_dict[student_email]
-        for key in self._students_dict:
-            if student_email == key:
+        # student = self._students_dict[student_email]
+        # for key in self._students_dict:
+        #     if student_email == key:
+        # True
 
+    def persist_students(self):
+        try:
+            folder_where_json_file_is = pathlib.Path(__file__).parent.parent
+            db_file = str(folder_where_json_file_is) + os.sep + "students.json"
+
+            if os.path.exists(db_file):
+                os.remove(db_file)
+            else:
+                raise Exception("File doesn't exist")
+
+            student_json = json.dumps(self._students_dict)
+
+            file = open("users.json", "a")
+            file.write(student_json)
+            file.close()
+            return "Success"
+
+        except Exception as e:
+            raise Exception("something went wrong, error is: {}".format(e))
 
 
 
