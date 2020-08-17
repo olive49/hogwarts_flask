3d78d906e48383c4fb92a4d78b9cf93107a13e8f
Becca <60694049+olive49@users.noreply.github.com>
2020-08-17 13:58:36 +0300

    add student to database, 3 db's

--- MySqlDataLayer.py
+++ MySqlDataLayer.py
@@ -34,57 +34,74 @@ class MySqlDataLayer(BaseDBLayer):
         finally:
             cursor.close()
 
-    def add_existing_skills(self, student):
+    def add_existing_skills(self, student, last_id):
         cursor = self.__mydb.cursor()
-        existing_list = []
         for skill in student["existing_magic_skills"]:
-            existing_skills = "INSERT INTO existing_skills (skill_name, skill_rank) VALUES (%s, %s)"
-            existing_val = (skill["Skill"], skill["Level"])
+            if skill["Skill"] == "Potion Making":
+                skill_id = 1
+            elif skill["Skill"] == "Spells":
+                skill_id = 2
+            elif skill["Skill"] == "Quidditch":
+                skill_id = 3
+            elif skill["Skill"] == "Apparate":
+                skill_id = 4
+            elif skill["Skill"] == "Metamorphmagi":
+                skill_id = 5
+            elif skill["Skill"] == "Parseltongue":
+                skill_id = 6
+            existing_skills = "INSERT INTO existing_skills (skill_id, skill_name, skill_rank, " \
+                              "student_id) VALUES (%s, %s, %s, %s)"
+            existing_val = (skill_id, skill["Skill"], skill["Level"], last_id)
             cursor.execute(existing_skills, existing_val)
-            self.__mydb.commit()
-            existing_last = cursor.lastrowid
-            existing_list.append(existing_last)
-        return existing_list
+        return
 
-    def add_desired_skills(self, student):
+    def add_desired_skills(self, student, last_id):
         cursor = self.__mydb.cursor()
-        desired_list = []
         for skill in student["desired_magic_skills"]:
+            if skill == "Potion Making":
+                skill_id = 1
+            elif skill == "Spells":
+                skill_id = 2
+            elif skill == "Quidditch":
+                skill_id = 3
+            elif skill == "Apparate":
+                skill_id = 4
+            elif skill == "Metamorphmagi":
+                skill_id = 5
+            elif skill == "Parseltongue":
+                skill_id = 6
             print("skill desire loop", skill)
-            desired_skills = "INSERT INTO desired_skills (skill_name) VALUES (%s)"
-            cursor.execute(desired_skills, (skill,))
-            self.__mydb.commit()
-            desired_last = cursor.lastrowid
-            desired_list.append(desired_last)
-        return desired_list
+            desired_skills = "INSERT INTO desired_skills (skill_id, skill_name, student_id) VALUES (%s, %s, %s)"
+            desired_val = (skill_id, skill, last_id)
+            cursor.execute(desired_skills, desired_val)
+        return
 
 
     def add_student(self, student):
         try:
+            self.__mydb.autocommit = False
             cursor = self.__mydb.cursor()
+            self.__mydb.start_transaction()
             sql = "INSERT INTO students (first_name, last_name, email, created_at, " \
                   "last_update) VALUES (%s, %s, %s, %s, %s)"
             val = (student["first_name"], student["last_name"], student["email"],
                    student["creation_time"], student["last_update"])
             cursor.execute(sql, val)
             last_id = cursor.lastrowid
-            existing = MySqlDataLayer.add_existing_skills(self, student)
-            desired = MySqlDataLayer.add_desired_skills(self, student)
-            for exist in existing:
-                for desire in desired:
-                    magic_skills = "INSERT INTO magic_skills (student_id, existing_skill_id, " \
-                           "desired_skill_id) VALUES (%s, %s, %s)"
-                    magic_val = (last_id, exist, desire)
-                    cursor.execute(magic_skills, magic_val)
-                    self.__mydb.commit()
+            MySqlDataLayer.add_existing_skills(self, student, last_id)
+            MySqlDataLayer.add_desired_skills(self, student, last_id)
+            self.__mydb.commit()
             print(cursor.rowcount, "record inserted.")
             return cursor.rowcount
 
         except Error as error:
             print("Failed to update record to database rollback: {}".format(error))
+            self.__mydb.rollback()
 
         finally:
-            cursor.close()
+            if(self.__mydb.is_connected()):
+                cursor.close()
+                self.__mydb.close()
 
 
     def remove_student(self, student):
