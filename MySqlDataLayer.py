841da71d69b57596876d6159c2e29f4c12aa84f1
Becca <60694049+olive49@users.noreply.github.com>
2020-08-18 14:13:23 +0300

    switched to dictionary

--- MySqlDataLayer.py
+++ MySqlDataLayer.py
@@ -19,6 +19,8 @@ class MySqlDataLayer(BaseDBLayer):
             password=config('PASSWORD'),
             database="hogwarts"
         )
+        self.__my_skill = {"Potion Making": 1, "Spells": 2, "Quidditch": 3,
+                "Apparate": 4, "Metamorphmagi": 5, "Parseltongue": 6}
 
     def shutdown_db(self):
         self.__mydb.close()
@@ -51,43 +53,24 @@ class MySqlDataLayer(BaseDBLayer):
 
     def add_existing_skills(self, student, last_id):
         cursor = self.__mydb.cursor()
-        for skill in student["existing_magic_skills"]:
-            if skill["Skill"] == "Potion Making":
-                skill_id = 1
-            elif skill["Skill"] == "Spells":
-                skill_id = 2
-            elif skill["Skill"] == "Quidditch":
-                skill_id = 3
-            elif skill["Skill"] == "Apparate":
-                skill_id = 4
-            elif skill["Skill"] == "Metamorphmagi":
-                skill_id = 5
-            elif skill["Skill"] == "Parseltongue":
-                skill_id = 6
+        for skill_name in student["existing_magic_skills"]:
+            for skill, level in self.__my_skill.items():
+                if skill_name["Skill"] == skill:
+                    skill_id = level
             existing_skills = "INSERT INTO existing_skills (skill_id, skill_name, skill_rank, " \
                               "student_id) VALUES (%s, %s, %s, %s)"
-            existing_val = (skill_id, skill["Skill"], skill["Level"], last_id)
+            existing_val = (skill_id, skill_name["Skill"], skill_name["Level"], last_id)
             cursor.execute(existing_skills, existing_val)
         return
 
     def add_desired_skills(self, student, last_id):
         cursor = self.__mydb.cursor()
-        for skill in student["desired_magic_skills"]:
-            if skill == "Potion Making":
-                skill_id = 1
-            elif skill == "Spells":
-                skill_id = 2
-            elif skill == "Quidditch":
-                skill_id = 3
-            elif skill == "Apparate":
-                skill_id = 4
-            elif skill == "Metamorphmagi":
-                skill_id = 5
-            elif skill == "Parseltongue":
-                skill_id = 6
-            print("skill desire loop", skill)
+        for skill_name in student["desired_magic_skills"]:
+            for skill, level in self.__my_skill.items():
+                if skill_name["Skill"] == skill:
+                    skill_id = level
             desired_skills = "INSERT INTO desired_skills (skill_id, skill_name, student_id) VALUES (%s, %s, %s)"
-            desired_val = (skill_id, skill, last_id)
+            desired_val = (skill_id, skill_name, last_id)
             cursor.execute(desired_skills, desired_val)
         return
 
@@ -197,46 +180,30 @@ class MySqlDataLayer(BaseDBLayer):
 
     def update_existing_skills(self, data, student_id):
         cursor = self.__mydb.cursor()
-        for skill in data["existing_magic_skills"]:
-            if skill["Skill"] == "Potion Making":
-                skill_id = 1
-            elif skill["Skill"] == "Spells":
-                skill_id = 2
-            elif skill["Skill"] == "Quidditch":
-                skill_id = 3
-            elif skill["Skill"] == "Apparate":
-                skill_id = 4
-            elif skill["Skill"] == "Metamorphmagi":
-                skill_id = 5
-            elif skill["Skill"] == "Parseltongue":
-                skill_id = 6
+        for skill_name in data["existing_magic_skills"]:
+            for skill, level in self.__my_skill.items():
+                if skill_name["Skill"] == skill:
+                    skill_id = level
             existing_skills = "UPDATE existing_skills " \
                               "SET skill_id = %s, skill_name = %s, skill_rank = %s " \
                               "WHERE " \
-                              "student_id = %s "
-            existing_val = (skill_id, skill["Skill"], skill["Level"], student_id)
+                              "student_id = %s " \
+                              "LIMIT 1"
+            existing_val = (skill_id, skill_name["Skill"], skill_name["Level"], student_id)
             cursor.execute(existing_skills, existing_val)
         return
 
     def update_desired_skills(self, data, student_id):
         cursor = self.__mydb.cursor()
-        for skill in data["existing_magic_skills"]:
-            if skill["Skill"] == "Potion Making":
-                skill_id = 1
-            elif skill["Skill"] == "Spells":
-                skill_id = 2
-            elif skill["Skill"] == "Quidditch":
-                skill_id = 3
-            elif skill["Skill"] == "Apparate":
-                skill_id = 4
-            elif skill["Skill"] == "Metamorphmagi":
-                skill_id = 5
-            elif skill["Skill"] == "Parseltongue":
-                skill_id = 6
-            desired_skills = "UPDATE desired_skills" \
-                              "SET skill_id = %, skill_name = %, " \
-                              "WHERE student_id = %"
-            desired_val = (skill_id, skill["Skill"], skill["Level"], student_id)
+        for skill_name in data["desired_magic_skills"]:
+            for skill, level in self.__my_skill.items():
+                if skill_name["Skill"] == skill:
+                    skill_id = level
+            desired_skills = "UPDATE desired_skills " \
+                             "SET skill_id = %s, skill_name = %s " \
+                             "WHERE " \
+                             "student_id = %s "
+            desired_val = (skill_id, skill_name["Skill"], student_id)
             cursor.execute(desired_skills, desired_val)
         return
 
